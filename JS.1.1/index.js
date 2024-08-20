ccf73bc34ff6d110e69b2da692266fab4f800d01
David Andersen <DAVID.ANDERSEN6113@STU.MTEC.EDU>
2024-08-19 21:44:44 -0600

    JS day one assignments 1-3

--- JS.1.1/index.js
+++ JS.1.1/index.js
@@ -29,47 +29,51 @@ console.log(`sum3 is ${sum3}`);
 // Make the name of the let variable aLet with any value
 // Make the name of the const variable aConst with any value
 // Make the name of the var variable aVar with any value
-
-
+let aLet ="welp";
+const aConst =22;
+var aVar ="bad idea";
 
 // Types
 // 2) Assign the variable below aString to any string
 // When someone says to assign they mean to put a value after the equals sign.
 
-let aString;
+let aString='a string';
 
 
 // 3) Assign the variable below aNumber to any number
 
-let aNumber;
+let aNumber = 22;
 
 
 // 4) Assign the variable below aBool to any boolean value
 
-let aBool;
+let aBool = true;
 
 
 // Operations
 // 5) Add two numbers together. Assign firstNumber 5 and secondNumber 30.
 // Add those two variables together and assign it to addedNumbers
 
-let firstNumber;
-let secondNumber;
+let firstNumber = 5;
+let secondNumber = 30;
 
-let addedNumbers;
+let addedNumbers = firstNumber + secondNumber;
+console.log(addedNumbers)
 
 
 //6) Add two strings together. Assign firstString 'Hello ' and secondString 'World'
 // Add those two variables together and assign it to addedStrings
 
-let firstString;
-let secondString;
+let firstString = 'Hello ';
+let secondString = 'World';
 
-let addedStrings;
+let addedStrings = firstString + secondString;
+console.log(addedStrings)
 
 //7) Below is a variable that is set to true. Flip it to false using the not operator and assign
 // it to lieDetector
 
 let iHaveMillions = true;
 
-let lieDetector;
+let lieDetector = !(iHaveMillions);
+console.log(lieDetector)
--- JS.1.3/index.js
+++ JS.1.3/index.js
@@ -4,7 +4,8 @@
 let yardWidth = 100;
 let yardLength = 15;
 
-let yardPerimeter;
+let yardPerimeter = (yardWidth * 2) + (yardLength * 2);
+console.log(yardPerimeter);
 
 
 // 2) With the yardPerimeter variable go ahead and use back ticks (``)
@@ -14,8 +15,8 @@ let yardPerimeter;
 // Don't remember how to do this? Go back to the first repl.it assignment from today
 // and look at the example problems console.log().
 
-let messagePerimeter;
-
+let messagePerimeter = `'you are going to need ${yardPerimeter}ft of fence'.`;
+console.log(messagePerimeter);
 // 3)One last thing. It is going to cost me $15 per foot of fence.
 // What is the total price I am going to have to pay?
 // Use the yardPerimeter variable and the pricePerFoot variable
@@ -23,4 +24,5 @@ let messagePerimeter;
 
 let pricePerFoot = 15;
 
-let totalPrice;
+let totalPrice = yardPerimeter * 15;
+console.log(totalPrice);
